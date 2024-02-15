12b30e951e63bf8b702448c1f17af631a19e433a
Mattias Berglund <mattias.berglund@arbetsformedlingen.se>
2024-02-15 08:31:41 +0100

    url + secret as env

--- src/lib/server/service-handler.ts
+++ src/lib/server/service-handler.ts
@@ -1,11 +1,8 @@
-const AUTH_TOKEN =
-  "aea0295be6d45c44e7cd16ba47fce6fa3c1d8184bb136a13539a59e58968bef32b866eda733465c8981de2d4223d6992821b976cb390895e3ebc07691a6bf0228a4be4168c237a366cd8767f78fe105688cb6e9867736ac920f0634e2a5cd321252e28c82e65fb9365eb055e0ec95628fa90c127f845634fdc14eec87fc20bbc";
+import { env } from "$env/dynamic/private";
 
 export const getPosts = async () => {
-  return [];
-
-  const res = await fetch("http://localhost:1337/api/blogs", {
-    headers: { Authorization: `Bearer ${AUTH_TOKEN}` },
+  const res = await fetch(`${env.CMS_URL}/api/blogs`, {
+    headers: { Authorization: `Bearer ${env.AUTH_TOKEN}` },
   });
 
   if (!res.ok) {
