aa62e81c9f9d64a14422ac1588c48c355bc42842
Fredric <fredric.farholt@arbetsformedlingen.se>
2024-07-23 12:44:45 +0200

    camelCase

--- src/lib/components/about/Team.svelte
+++ src/lib/components/about/Team.svelte
@@ -3,18 +3,18 @@
   import MemberCard from "./MemberCard.svelte";
   import type { TeamData } from "$lib/interfaces";
 
-  export let Team: TeamData;
+  export let teamData: TeamData;
 </script>
 
 <Container backgroundColor="bg-white" foreGroundColor="bg-default-gray-100">
   <div class="max-w-[1362px] m-auto space-y-12 px-10">
     <h3 class="text-4xl lg:text-6xl pt-24 lg:pt-0 font-bold text-slate-800">
-      {Team.header}
+      {teamData.header}
     </h3>
     <div
       class="flex overflow-x-auto sm:overflow-hidden sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10 pb-12 sm:pb-0"
     >
-      {#each Team.content as member}
+      {#each teamData.content as member}
         <MemberCard {member} />
       {/each}
     </div>
