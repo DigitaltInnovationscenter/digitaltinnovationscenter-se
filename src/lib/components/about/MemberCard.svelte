cc8574951dd1a3a346d017620bbc437af2193e84
Fredric <fredric.farholt@arbetsformedlingen.se>
2024-08-15 13:46:18 +0200

    image logic for members

--- src/lib/components/about/MemberCard.svelte
+++ src/lib/components/about/MemberCard.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { TeamMember } from "$lib/interfaces";
+  import { PUBLIC_CMS_URL } from "$env/static/public";
 
   export let member: TeamMember = {
     Name: "",
@@ -12,9 +13,17 @@
 <div class="flex flex-col space-y-2 min-w-[234px]">
   <div
     style="aspect-ratio: 1.2/1;"
-    class="w-full bg-gray-100 border border-gray-300 rounded-xl"
+    class="w-full bg-gray-100 border border-gray-300 rounded-xl relative overflow-hidden"
   >
-    <!-- TODO: Build image logic as soon as profile images are available and uploaded -->
+    {#if member.MemberImage.data}
+      <img
+        src={PUBLIC_CMS_URL + member.MemberImage.data?.attributes.url}
+        alt="Bild som representerar rubriken medlem"
+        class="min-w-full min-h-full absolute left-1/2 transform -translate-x-1/2 top-1/2 -translate-y-1/2"
+      />
+    {:else}
+      <!-- Placeholder image -->
+    {/if}
   </div>
   <div class="pt-4">
     <h3 class="font-bold text-lg text-gray-700">{member.Name}</h3>
